e7d1791d76f4a66abad488a38887de7c4f24bfe1
Zack Elliott <hubridnoxx@gmail.com>
2015-08-28 21:41:28 -0400

    Added Babylonian sqrt algo

---
+++ algos/sqrt.js
@@ -0,0 +1,24 @@
+// Babylonian method implementation
+function sqrtFn(n) {
+  var ERROR_MARGIN = 0.01;
+
+  if (n < 0) {
+    console.log(n + ' should be greater than 0');
+    return;
+  }
+
+  var upperBound = 1;
+  var lowerBound = n;
+
+  while(Math.abs(upperBound - lowerBound) > ERROR_MARGIN) {
+    console.log(upperBound, lowerBound);
+    upperBound = (upperBound + lowerBound) / 2;
+    lowerBound = n / upperBound;
+  }
+
+  return upperBound;
+}
+
+console.log(sqrtFn(100));
+console.log(sqrtFn(26));
+console.log(sqrtFn(12388));
