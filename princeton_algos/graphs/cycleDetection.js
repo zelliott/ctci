c1fd1c05bcc870d60517fcc2850b5b4b1c8f39d0
Zack Elliott <hubridnoxx@gmail.com>
2015-08-30 18:59:10 -0400

    Finished cycleDetection

---
+++ princeton_algos/graphs/cycleDetection.js
@@ -0,0 +1,58 @@
+var Graph = require('../../utils/graph.js');
+
+function cycleDetection(graph) {
+  var visited = {};
+  var stack = [];
+  var adjacencyList = graph.getNodes();
+  var nodes = Object.keys(adjacencyList);
+
+  stack.push({
+    'node': nodes[0]
+  });
+
+  // While we still have nodes in the stack.
+  while (stack.length > 0) {
+    var nodeInfo = stack.pop();
+    var cameFrom = nodeInfo.cameFrom;
+    var node = nodeInfo.node;
+
+    // If not visited yet
+    if (typeof visited[node] === 'undefined') {
+      console.log('visited: ' + node);
+      visited[node] = true;
+
+      // Push neighbors onto the stack
+      for (var i = 0; i < adjacencyList[node].length; i++) {
+
+        // Don't add the neighbor we *just* came from...
+        // FIXME: This '!=' is bad style, used because we are comparing 1 with '1'
+        // for some reason...
+        if (typeof cameFrom === 'undefined' || adjacencyList[node][i] != cameFrom) {
+          stack.push({
+            'node': adjacencyList[node][i],
+            'cameFrom': node
+          });
+        }
+      }
+    } else if (visited[node]) {
+      return false;
+    }
+
+  }
+
+  return true;
+}
+
+var graphA = new Graph();
+
+graphA.addNode(1);
+graphA.addNode(2);
+graphA.addNode(3);
+graphA.addNode(4);
+
+graphA.addEdge(1, 2);
+graphA.addEdge(2, 3);
+graphA.addEdge(3, 4);
+graphA.addEdge(2, 4);
+
+console.log(cycleDetection(graphA));
